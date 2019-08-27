425871cf59f783d20cc192268763017e5c6f96f3
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-26 22:55:13 -0500

    restrict user profile to logged in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
 
 
   # GET /users/1
@@ -35,4 +36,11 @@ class UsersController < ApplicationController
     def user_params
       params.fetch(:user, {})
     end
+
+    def logged_in_user
+			unless logged_in?
+				flash[:danger] = "Please log in."
+				redirect_to login_url
+			end
+		end
 end
