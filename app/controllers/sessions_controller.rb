179de35cb9bb8f4322cc61283d4ab0a48c6a0f97
trox115 <tofernandes67@gmail.com>
2019-08-27 20:48:08 +0100

    added login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
     def new
     end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+          flash[:success] = 'Thank you for signing in!'
+          sign_in user
+          redirect_to root_path        
+        else
+          flash.now[:error] = 'Invalid email/password combination'
+          render 'new'
+        end
+      end
+
+      def destroy
+        sign_out
+        redirect_to root_path
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_135737) do
+ActiveRecord::Schema.define(version: 2019_08_27_191559) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_08_26_135737) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_token"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
