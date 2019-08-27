de65611142759dd5b1c2a4a2c6677de3a315697e
trox115 <tofernandes67@gmail.com>
2019-08-27 20:15:24 +0100

    added Login Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
-  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/login',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
