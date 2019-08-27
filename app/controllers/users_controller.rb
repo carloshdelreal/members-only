ad30a9af4b27967b9b6b27bb99e76f1c97002188
trox115 <tofernandes67@gmail.com>
2019-08-28 00:18:50 +0100

    users can signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    
     if @user.save
       redirect_to @user, notice: 'User was successfully created.'
       
@@ -34,7 +32,7 @@ class UsersController < ApplicationController
   
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
 
     def logged_in_user
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   
-  #match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   delete '/logout',  to: 'sessions#destroy'
   
-  #match '/signin',  to: 'sessions#new',         via: 'get'
+
   get  '/signup',  to: 'users#new'
   
   post '/signup',  to: 'users#create'
   get '/profile', to: 'users#show'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   resources :posts,     only: [:new, :create, :index]
   resources :users, only: [:new, :create, :show]  
   root 'posts#index'
