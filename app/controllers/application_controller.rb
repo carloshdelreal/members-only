4e47dc2775acab135d65ac8b1bdacc1072ce8a35
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-29 19:28:18 -0500

    take out the logic form the helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,23 @@ class ApplicationController < ActionController::Base
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    self.current_user = user
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,21 +15,5 @@ module SessionsHelper
       end
     end
   end
-  def sign_out
-    current_user.update_attribute(:remember_token,
-                                  User.digest(User.new_remember_token))
-    cookies.delete(:remember_token)
-    self.current_user = nil
-  end
-
-  def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
-    self.current_user = user
-  end
-
-  def current_user=(user)
-    @current_user = user
-  end
+  
 end
