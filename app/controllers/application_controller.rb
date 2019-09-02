f081fc1004b981d7b98c513bc0de9e71575c6f0f
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-02 14:55:35 -0500

    error when log out fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,9 +29,8 @@ class ApplicationController < ActionController::Base
   end
 
   def sign_out
-    current_user.update_attribute(:remember_token,
-                                  User.digest(User.new_remember_token))
-    cookies.delete(:remember_token)
-    self.current_user = nil
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
